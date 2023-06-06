859c557b43ef3ec216ae25ebcd6095774bdd314f
Ofek Sror <94007441+Ofeksror@users.noreply.github.com>
2023-06-06 18:04:51 +0300

    Greatest dividor of strings

--- ArraysAndStrings/gcdOfStrings.ts
+++ ArraysAndStrings/gcdOfStrings.ts
@@ -2,30 +2,23 @@
 // Easy
 // https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
 
+// Euclidean algorithm 
+
+
 function gcdOfStrings(str1: string, str2: string): string {
-    if (str1.length == str2.length) {
-        if (str1 == str2)
-            return str2;
+    if (str1 + str2 != str2 + str1)
         return "";
+    
+    let gcd = (a: number, b: number): number => {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
     }
 
-    const dividerLength = str2.length - (str1.length % str2.length);
-    const divider = str2.substring(0, dividerLength);
+    let x = gcd(str1.length, str2.length);
 
-    // Check if str2 contains the divider one or more times
-    for (let i = 0; i + dividerLength < str2.length; i += dividerLength) {
-        if (str2.substring(i, i + dividerLength) != divider)
-            return "";
-    }
-
-    // Check if st1 contains the divider one or more times
-    for (let i = 0; i + dividerLength <= str1.length; i += dividerLength) {
-        if (str1.substring(i, i + dividerLength) != divider)
-            return "";
-    }
-
-    return divider;
-};
+    return str1.substring(0, x);
+}
 
 (() => {
     console.log((gcdOfStrings("ABCABC", "ABC") === "ABC") ? "✅ | TEST #1" : "❌ | TEST #1");
